096ca34c4bcbef6cc11e189194d404acc899e91f
Munna Islam <116387452+Munnaz23@users.noreply.github.com>
2024-01-17 22:55:32 +0600

    Add files via upload
    first commit

---
+++ type_cast.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    int a = 'a';
+    cout<<a<<endl;
+
+    char ch = 98;
+    cout<<ch<<endl;
+
+    return 0;
+    */
+
+    unsigned int  a=-123456;
+    cout<<a<<endl;
+
+}
